eb9a6fd7c9100d86300be25c1bb91ea132d64ab3
Cuiboy <patrickcui1122@icloud.com>
2019-02-26 11:09:32 -0800

    - athletic update

--- PHS App/AthleticsViewController.swift
+++ PHS App/AthleticsViewController.swift
@@ -19,7 +19,7 @@ class AthleticsViewController: UIViewController, UICollectionViewDelegate, UICol
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var segmentioView: Segmentio!
     var content = [SegmentioItem]()
-    let allIcons = ["Baseball", "Boys Basketball", "Track", "Football", "Girls Golf", "Girls Lacrosse", "Boys Soccer", "Softball", "Girls Tennis", "Boys Swim", "Cross Country", "Girls Volleyball", "Boys Water Polo","Wrestling", "Girls Basketball", "Boys Golf", "Boys Lacrosse", "Girls Soccer", "Boys Tennis", "Girls Water Polo", "Girls Swim", "Boys Volleyball"]
+    let allIcons = ["Baseball", "Boys Basketball", "Track", "Football", "Girls Golf", "Girls Lacrosse", "Boys Soccer", "Softball", "Girls Tennis", "Swim", "Cross Country", "Girls Volleyball", "Boys Water Polo","Wrestling", "Girls Basketball", "Boys Golf", "Boys Lacrosse", "Girls Soccer", "Boys Tennis", "Girls Water Polo", "Boys Volleyball"]
     let allImages: [UIImage] = [
         UIImage(named: "Baseball")!,
         UIImage(named: "Basketball")!,
@@ -47,7 +47,7 @@ class AthleticsViewController: UIViewController, UICollectionViewDelegate, UICol
     
     let fallIndex: [Int] = [3, 4, 8, 10, 11, 12]
     let winterIndex: [Int] = [1, 6, 17, 14, 13, 19]
-    let springIndex: [Int] = [0, 2, 5, 7, 9, 15, 16, 18, 20, 21]
+    let springIndex: [Int] = [0, 2, 5, 7, 9, 15, 16, 18, 20]
     
     var fallIcons = [String]()
     var winterIcons = [String]()
